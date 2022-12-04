9c6a4b0d14c1810cb1bb0fc652b8d743d9be27c8
Kerby22 <test20072009@yandex.ru>
2022-12-05 01:30:15 +0700

    Отработал задачу 1

--- zadacha1/Program.cs
+++ zadacha1/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿//start
+Console.Clear();
+int numb0 = 22;
+int numb1 = 3;
+int numb2 = 9;
+int max = numb0;
+if (max < numb1)
+{
+  max = numb1;  
+}
+if (max < numb2)
+{
+    max = numb2;
+}
+Console.Write("максимальное из этих чисел - ");
+Console.Write(max);
+//stop
