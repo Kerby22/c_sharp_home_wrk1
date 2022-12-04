50f2171dc8977de1cbf87262dbd3d6d277671245
Kerby22 <test20072009@yandex.ru>
2022-12-05 02:59:44 +0700

    ne smog ostavit bez kommenta

--- zadacha2/Program.cs
+++ zadacha2/Program.cs
@@ -1,8 +1,9 @@
 ﻿//start
 Console.Clear();
-double chetnechet = 6;
+double chetnechet = -3;
 if(chetnechet%2==0)
 {
     Console.Write($"число {chetnechet} является четным");
     }
+    Console.Write("нечетное число (чтобы программа внезапно не останавливалась)");
 //stop
